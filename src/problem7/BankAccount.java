34857c75d8d2e034346d1f4b39e449bc108254cf
Archit Saxena <Archit.Saxena@godigit.com>
2024-08-01 17:32:36 +0530

    [Archit]ADD: banking system

---
+++ src/problem7/BankAccount.java
@@ -0,0 +1,21 @@
+package problem7;
+
+public class BankAccount {
+    public double balance;
+    public BankAccount(double initialBalance) {
+        this.balance = initialBalance;
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+
+    public void withdraw(double amount) throws InsufficientFundsException {
+        if(amount > balance) {
+            throw new InsufficientFundsException("Insufficient funds. Your balance is " + balance);
+        } else {
+            balance -= amount;
+            System.out.println("Withdrawal completed! The new balance is " + balance);
+        }
+    }
+}
