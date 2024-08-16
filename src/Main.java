93b925a873b52b844cba9a348648e83e3efec564
Archit Saxena <Archit.Saxena@godigit.com>
2024-08-16 11:25:04 +0530

    [Archit]ADD: establish connection

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+import java.sql.*;
+
+public class Main {
+    public static void main(String[] args) throws SQLException {
+        String url = "jdbc:postgresql://localhost:5432/test_db";
+        String username = "postgres";
+        String password = "Archit@436";
+
+        try (Connection con = DriverManager.getConnection(url, username, password)) {
+            System.out.println("Connection established successfully");
+        } catch (SQLException e) {
+            System.out.println("Error: " + e.getMessage());
+        }
+    }
+}
