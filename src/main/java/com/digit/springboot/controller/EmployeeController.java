06d1e55ce910c3aa40fda632ddb84fbab21ba3ab
Archit Saxena <Archit.Saxena@godigit.com>
2024-08-23 15:20:06 +0530

    [Archit]ADD: added swagger config

--- src/main/java/com/digit/springboot/controller/EmployeeController.java
+++ src/main/java/com/digit/springboot/controller/EmployeeController.java
@@ -2,6 +2,7 @@ package com.digit.springboot.controller;
 
 import com.digit.springboot.model.Employee;
 import com.digit.springboot.service.EmployeeService;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -10,14 +11,15 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api/employees")
+@CrossOrigin(origins = "http://localhost:8080")
 public class EmployeeController {
     @Autowired
     private EmployeeService employeeService;
-
     @PostMapping("/")
     public Employee createEmployee(@RequestBody Employee employee) {
         return employeeService.createEmployee(employee);
     }
+
     @GetMapping("/{id}")
     public ResponseEntity<Employee> getEmployeeById(@PathVariable("id") Long id) {
         return employeeService.getEmployeeById(id)
